1a42bb46059449caa2ea7118c48fd9f9d71dd521
unknown <irina.kiselevitch@yandex.ru>
2022-10-24 20:58:11 +0700

    первая задача

--- Program.cs
+++ Program.cs
@@ -1,20 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-//Console.WriteLine("Hello, World!");
-Console.Write ("Введите размер массива: ");
-int size = Convert.ToInt32(Console.ReadLine());
-int [] array = new int [size];
-for (int i =0;i<array.Length;i++)
+﻿Console.Write("Введите элементы(через пробел): ");
+int[] arr = Array.ConvertAll(Console.ReadLine().Split(), int.Parse);
+int count = 0;
+ 
+for (int i = 0; i < arr.Length; i++)
 {
-    array[i]=new Random().Next(1,100); 
-    Console.Write(array[i] + " ");
+    if (arr[i] > 0)
+    {
+        count++;
+    }
 }
-Console.WriteLine();
-int result = 0;
-for (int i =0;i<array.Length;i++)
-{
-    if (i %2 !=0)
-{
-    result += array[i];
-}
-Console.WriteLine($"Сумма элементов на нечетных позициях = {result}");
-}
+ 
+Console.WriteLine($"Кол-во элементов > 0: {count}");
